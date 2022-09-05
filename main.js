a1b13806547e8e77eadeabc074939e02f7a70de5
Agustin Savino <110129241+Airiled@users.noreply.github.com>
2022-09-05 18:34:46 -0300

    Creamos productos de forma dinamica con su html y css mediante JS

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-//En el primer ejercicio se nos pidio que al tocar el mail que aparece en la pagina se nos abriria el menu del usuario
-//el codigo comentado debajo es como lo realice sin saber que existia la linea que se realizo despues de 'desktopMenu.classList.toggle('inactive');'
-
 const desktopMenu = document.querySelector('.desktop-menu');
 const userEmail = document.querySelector('.navbar-email');
 
@@ -10,7 +7,7 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const carritoIcon = document.querySelector('.navbar-shopping-cart')
 const carritoMenu = document.querySelector('.product-detail')
 
-//let aux = true;
+const cardsContainer = document.querySelector('.cards-container');
 
 console.log('entramos al programa');
 
@@ -43,3 +40,59 @@ function toggleAsideMenu(){
 
 }
 
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 100,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'TV',
+    price: 400,
+    img: 'https://images.pexels.com/photos/5202925/pexels-photo-5202925.jpeg?auto=compress&cs=tinysrgb&w=600'
+});
+productList.push({
+    name: 'Notebook',
+    price: 500,
+    img: 'https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600'
+});
+
+function crearProductos(){
+    for(product of productList){
+        //Primero creamos la etiqueta correspondiente y se la asignamos a una variable
+        //Luego le agregamos la/las clases que sean necesarias a la etiqueta para que queden como en el ejemplo
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card')
+    
+        const imgProduct = document.createElement('img');
+        imgProduct.setAttribute('src', product.img);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const price = document.createElement('p');
+        price.innerText = "$" + product.price;
+        const name = document.createElement('p');
+        name.innerText = product.name;
+    
+        const figureImg = document.createElement('figure');
+        const imgFigure = document.createElement('img');
+        imgFigure.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        //Comenzamos a acomodar de forma cascada las etiquetas colocandolas como corresponden
+
+        figureImg.append(imgFigure);
+    
+        productInfoDiv.append(price, name);
+    
+        productInfo.append(productInfoDiv, figureImg);
+    
+        productCard.append(imgProduct, productInfo);
+    
+        cardsContainer.append(productCard);
+    
+    }
+}
